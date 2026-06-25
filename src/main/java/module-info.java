28825bd477ed3742d2452201c6a94141010095c0
Leyuanyo <Juanjosemochomio@gmail.com>
2026-06-25 13:33:41 -0500

    Fixed tests and fixed route C not found when the program started

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module com.example.miniproyecto_50zo {
     requires javafx.controls;
     requires javafx.fxml;
-    requires org.junit.jupiter.api;
 
     opens com.example.miniproyecto_50zo to javafx.fxml;
     opens com.example.miniproyecto_50zo.controller to javafx.fxml;
